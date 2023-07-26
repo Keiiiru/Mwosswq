89121b0350ec0e5189b907f15090716b135973e3
Keiiiru <114057155+Keiiiru@users.noreply.github.com>
2023-07-27 00:34:49 +0300

    upload

---
+++ dbreg/datebook/db.py
@@ -0,0 +1,35 @@
+import os 
+import sqlite3 
+
+
+db = sqlite3.connect(os.path.join('datebook.db'))
+
+def _get_cursor() -> sqlite3.Cursor:
+    cursor = db.cursor()
+    return cursor
+
+def _init_db():
+    cursor = _get_cursor()
+    stmt = """
+            create table if not exists date(
+                id integer primary key,
+                Day_of_week varchar(255),
+                Activity_for_the_day text,
+                user_id bigint
+            );
+    """
+    cursor.executescript(stmt)
+    db.commit()
+
+def add_mark(Day_of_week: str, Activity_for_the_day: str, user_id: int):
+    cursor = _get_cursor()
+    stmt = "insert into date(Day_of_week, Activity_for_the_day, user_id) values (?, ? ,?)"
+    cursor.execute(stmt, (Day_of_week,Activity_for_the_day,user_id))
+    db.commit()
+
+def get_marks(user_id: int):
+    cursor = _get_cursor()
+    stmt = "select*from date where user_id = ? " 
+    res = cursor.execute(stmt, (user_id, ))
+    data = res.fetchall() 
+    return data
