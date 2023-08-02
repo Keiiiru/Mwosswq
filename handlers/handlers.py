87d7ad56e880b826a33e2bad419a316daa1e6e69
Keiiiru <114057155+Keiiiru@users.noreply.github.com>
2023-08-02 11:10:27 +0300

    create handlers

---
+++ handlers/handlers.py
@@ -0,0 +1,44 @@
+import keyboards
+
+
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import Text
+from start import bot
+
+dp = Dispatcher(bot, strage=MemoryStorage())
+
+
+@dp.message_handler(commands=["start", "начать"])
+async def cmd_start(message: types.Message):
+    await message.answer("Hey?\n How can I help you?", reply_markup=keyboards.menu)
+
+
+@dp.message_handler(Text(equals="Contacts"))
+async def buttons(m: types.Message):
+    await m.answer("Contacts:", reply_markup=keyboards.contacts)
+
+
+@dp.message_handler(Text(equals="Command help"))
+async def cmd_list(m: types.Message):
+    await m.answer(
+        """
+        available commands: \n
+
+        """,
+        reply_markup=keyboards.cmd_list,
+    )
+
+
+@dp.message_handler(Text(equals="Back to menu"))
+async def menu(m: types.Message):
+    await m.answer("Hey?\n How can I help you?", reply_markup=keyboards.menu)
+
+
+@dp.message_handler(Text(equals="Datebook"))
+async def datebook(m: types.Message):
+    await m.answer(
+        "Will we create a new mark or browse old one?",
+        reply_markup=keyboards.planner_menu,
+    )
--- start.py
+++ start.py
@@ -2,6 +2,7 @@ import keyboards
 
 
 from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters import Text
 from aiogram import Bot, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from cfg.config_reader import config
@@ -13,32 +14,15 @@ bot = Bot(token=config.tg_bot.token)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(commands=["start", "начать"])
-async def cmd_start(message: types.Message):
-    await message.answer("Hey?\n How can I help you?", reply_markup=keyboards.menu)
-
-
-@dp.message_handler(commands=["Contacts"])
-async def buttons(m: types.Message):
-    await m.answer("Contacts:", reply_markup=keyboards.contacts)
-
-
-@dp.message_handler(commands=["Command help"])
-async def cmd_list(m: types.Message):
-    await m.answer(
-        """
-        available commands: \n
-
-        """,
-        reply_markup=keyboards.cmd_list,
-    )
-
-
 async def on_startup(_: Dispatcher):
     _init_db()
 
 
 if __name__ == "__main__":
+    from handlers import setup as setup_handlers
+
+    setup_handlers()
+
     register_edit_handler(dp)
 
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
